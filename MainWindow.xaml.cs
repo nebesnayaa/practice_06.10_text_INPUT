6c8d87a10eb8de885a0694d917f0d5058ad9b125
nebesnayaa <s.nebesnaya5@gmail.com>
2024-01-03 02:52:56 +0200

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+
+namespace practice_06._10_text_INPUT
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void inputTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!IsLetter(e.Text))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private bool IsLetter(string input)
+        {
+            // Перевірка, чи введено літеру [A-Z] або [А-Я]
+            return Regex.IsMatch(input, @"^[A-ZА-Я]$");
+        }
+    }
+}
